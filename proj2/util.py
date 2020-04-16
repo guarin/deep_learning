747973b455641ae81f3545599afc14123f8348ed
gfepfl <guarin.fluck@epfl.ch>
2020-04-16 16:41:57 +0200

    added required test code

--- proj2/util.py
+++ proj2/util.py
@@ -2,7 +2,13 @@ import torch
 import math
 
 
-def generate_samples(count):
-    input_ = torch.empty((count, 2)).uniform_(0, 1)
-    label = (input_.pow(2).sum(1) < 1 / (2 * math.pi)).type(torch.long)
-    return input_, label
+def generate_samples(count, one_hot=False):
+    input = torch.empty((count, 2)).uniform_(0, 1)
+    if one_hot:
+        target = torch.empty((count, 2)).zero_()
+        mask = input.pow(2).sum(1) < 1 / (2 * math.pi)
+        target[~mask, 0] = 1
+        target[mask, 1] = 1
+    else:
+        target = (input.pow(2).sum(1) < 1 / (2 * math.pi))
+    return input, target.long()

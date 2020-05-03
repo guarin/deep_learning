b86f20b3c50dc84c798833b44c68f79ba2641c52
Oriane Peter <oriane.peter@epfl.ch>
2020-05-03 17:03:25 +0200

    nice plot + ws in foward

--- proj1/weight_sharing.py
+++ proj1/weight_sharing.py
@@ -24,31 +24,34 @@ class LeNetLike(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(10, 1)
 )
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
+    def forward(self, input_):
+        output = []
+        for i in range(2):
+            x = input_[:,i,...].unsqueeze(1)
+            x = self.features(x)
+            x = x.view(x.size(0), -1)
+            x = self.classifier(x)
+            output.append(x)
+        output = torch.sigmoid(output[1] - output[0]).squeeze()
+        return output
     
 ######################################################################################
 
 
-# Accuracy for the baseline trains to get the classes then compare predicted class
+# Accuracy for the baseline trains to get the classes then compare predicted class
 def accuracy(model,inputs,targets):
     """ INPUT : 
         - model: model that predicts the digit values 
         - input_long: the input of the format 2000 x 10
         - tagets: ground truth of the pairs comparaison with format 1000 """
-    
-    # get long inputs
-    inputs_long = torch.cat((inputs[:,0,...],inputs[:,1,...]),0).unsqueeze(1)
+
     # Predict class of inputs
-    preds_long = model(inputs_long)
-    # Compare the pairs
-    preds = (preds_long[:1000,:] <= preds_long[1000:,:]).long().squeeze()
+    preds = model(inputs).round()
     # Compute accuracy
     accuracy = (preds == targets).long().sum().item()/len(preds)
     return accuracy 
+
+######################################################################################
 
 def train(model, train_input, train_target, mini_batch_size, verbose = False):
     criterion = nn.MSELoss()
@@ -57,11 +60,8 @@ def train(model, train_input, train_target, mini_batch_size, verbose = False):
         sum_loss = 0
         for b in range(0, train_input.size(0), mini_batch_size):
             optimizer.zero_grad()
-            inputs = train_input.narrow(0, b, mini_batch_size)
-            output_1 = model(inputs[:,0,...].unsqueeze(1))
-            output_2  = model(inputs[:,1,...].unsqueeze(1))
-            preds = F.sigmoid(output_2 - output_1).squeeze()
-            loss = criterion(preds, train_target.narrow(0, b, mini_batch_size))
+            output = model(train_input.narrow(0, b, mini_batch_size))
+            loss = criterion(output, train_target.narrow(0, b, mini_batch_size).float())
             sum_loss = sum_loss + loss.item()
             loss.backward()
             optimizer.step()

50aa03505cee3aa862f4f79e28fd346b080c3d02
gfepfl <guarin.fluck@epfl.ch>
2020-05-16 11:56:17 +0200

    changed radius of circle to get balanced classes

--- proj2/util.py
+++ proj2/util.py
@@ -6,9 +6,9 @@ def generate_samples(count, one_hot=False):
     input = torch.empty((count, 2)).uniform_(0, 1)
     if one_hot:
         target = torch.empty((count, 2)).zero_()
-        mask = input.pow(2).sum(1) < 1 / (2 * math.pi)
+        mask = input.pow(2).sum(1) < 2 / math.pi
         target[~mask, 0] = 1
         target[mask, 1] = 1
     else:
-        target = (input.pow(2).sum(1) < 1 / (2 * math.pi))
+        target = input.pow(2).sum(1) < 2 / math.pi
     return input, target.long()
